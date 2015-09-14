a558b299a1b3a9ffdbc8d9d5160ef23963a843b7
Jordan Olshevski <jordanolshevski@gmail.com>
2015-09-13 21:20:54 -0700

    Add test for fileurl input validation

--- api/api_test.go
+++ api/api_test.go
@@ -10,9 +10,17 @@ func TestValidateConfig(t *testing.T) {
 		t.Error("validateConfig should return an error when the modulepath hasn't been set")
 	}
 
-	subject := New()
-	subject.Config["modulepath"] = "stub modulepath"
-	if subject.validateConfig() != nil {
-		t.Error("validateConfig should return nil when the modulepath has been set")
+	subject1 := New()
+	subject1.Config["modulepath"] = "stub modulepath"
+	subject1.Config["fileurl"] = "stub fileurl"
+	if subject1.validateConfig() != nil {
+		t.Error("validateConfig should return nil when the modulepath and fileurl have been set")
 	}
+
+	subject2 := New()
+	subject2.Config["modulepath"] = "stub modulepath"
+	if subject2.validateConfig().Error() != "Fileurl must be set before starting the API server" {
+		t.Error("validateConfig should return an error when the fileurl hasn't been set")
+	}
+
 }
