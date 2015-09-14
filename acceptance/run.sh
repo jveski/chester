f5a74a996c1d4503ae55b574cd6584b899e477fb
Jordan Olshevski <jordanolshevski@gmail.com>
2015-09-13 23:29:01 -0700

    Print versions of Puppet and nginx

--- acceptance/run.sh
+++ acceptance/run.sh
@@ -3,11 +3,11 @@
 ###########################
 # Install Puppet and Nginx
 ###########################
-sudo apt-get update
-sudo apt-get install nginx -y
 wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
 sudo dpkg -i puppetlabs-release-precise.deb
+sudo apt-get update
 sudo apt-get install puppet -y
+sudo apt-get install nginx -y
 
 ###########################
 # Get a random module from the forge
@@ -26,10 +26,12 @@ go run main.go -modulepath acceptance/v3/files &
 ###########################
 # Start nginx
 ###########################
+/usr/sbin/nginx -v
 /usr/sbin/nginx -p acceptance -c nginx.conf
 
 
 ###########################
 # Attempt to install module from the API
 ###########################
+puppet --version
 puppet module install puppetlabs/apache --module_repository http://localhost:8080
